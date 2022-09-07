25e9be43faab2a4101728ac87b6b4242d498560b
Huy Ngo <huyngo878@yahoo.com>
2022-09-06 19:27:19 -0500

    TestPart3

--- client.py
+++ client.py
@@ -1,5 +1,6 @@
 import pygame
 import os
+import sys
 ##using the class "Network" from the file network.py
 from network import Network
 
@@ -19,6 +20,41 @@ pygame.display.set_caption("Client")
 
 clientNum = 0
 
+class playerGani(pygame.sprite.Sprite):
+    def __init__(self, pos_x, pos_y):
+        super().__init__()
+        self.sprites = []
+        self.is_animating = False
+        self.sprites.append(pygame.image.load(os.path.join('maps', 'Hobbit - run1.png')))
+        self.sprites.append(pygame.image.load(os.path.join('maps', 'Hobbit - run2.png')))
+        self.sprites.append(pygame.image.load(os.path.join('maps', 'Hobbit - run3.png')))
+        self.sprites.append(pygame.image.load(os.path.join('maps', 'Hobbit - run4.png')))
+        self.sprites.append(pygame.image.load(os.path.join('maps', 'Hobbit - run5.png')))
+        self.sprites.append(pygame.image.load(os.path.join('maps', 'Hobbit - run6.png')))
+        self.sprites.append(pygame.image.load(os.path.join('maps', 'Hobbit - run7.png')))
+        self.sprites.append(pygame.image.load(os.path.join('maps', 'Hobbit - run8.png')))
+        self.sprites.append(pygame.image.load(os.path.join('maps', 'Hobbit - run9.png')))
+        self.sprites.append(pygame.image.load(os.path.join('maps', 'Hobbit - run10.png')))
+        self.current_sprite = 0
+        self.image = self.sprites[self.current_sprite]
+
+        self.rect = self.image.get_rect()
+        self.rect.topleft = [pos_x, pos_y]
+
+    def animate(self):
+        self.is_animating = True
+
+    
+    def update(self):
+        if self.is_animating == True:
+            self.current_sprite += 0.2
+
+        if self.current_sprite >= len(self.sprites):
+            self.current_sprite = 0
+            self.is_animating = False
+
+        self.image = self.sprites[int(self.current_sprite)]
+
 class Player():
     def __init__(self, x, y, width, height, color):
         self.x = x
@@ -37,6 +73,7 @@ class Player():
             self.x -= VELOCITY
         if keys_pressed[pygame.K_d]: ## and self.x + VELOCITY + self.width< WIDTH - 20: ##right
             self.x += VELOCITY
+            player.animate()
         if keys_pressed[pygame.K_w]: ## and self.y - VELOCITY > - 25: ##up
             self.y -= VELOCITY
         if keys_pressed[pygame.K_s]: ##and self.y + VELOCITY + self.height < HEIGHT - 20: ##down
@@ -57,31 +94,38 @@ def make_pos(tup):
 def drawDisplay(DISPLAY, player, player2):
 
     DISPLAY.blit(MAP_IMAGE, (-50,0))
+    moving_sprites.draw(DISPLAY)
+    moving_sprites.update()
     player.draw(DISPLAY)
-    player2.draw(DISPLAY)
+    
     pygame.display.update()
+
+moving_sprites = pygame.sprite.Group()
+player = playerGani(100,100)
+moving_sprites.add(player)
 
 def main():
     run = True
     n = Network()
-    startPos = read_pos(n.getPos()) 
-    c = Player(startPos[0], startPos[1], 20, 20, (0,255,0))
+    #startPos = read_pos(n.getPos()) 
+    c = Player(0, 0, 20, 20, (0,255,0))
     c2 = Player(0, 0, 20, 20, (0,255,0))
     clock = pygame.time.Clock()
 
     while run:
         clock.tick(FPS)
-        c2Pos = read_pos(n.send(make_pos((c.x, c.y))))
-        c2.x = c2Pos[0]
-        c2.y= c2Pos[1]
-        c2.update()
+        #c2Pos = read_pos(n.send(make_pos((c.x, c.y))))
+        #c2.x = c2Pos[0]
+        #c2.y= c2Pos[1]
+        #c2.update()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
                 pygame.quit()
+                sys.exit()
         
         c.char_movement()
-        drawDisplay(DISPLAY, c, c2)
+        drawDisplay(DISPLAY, c, player)
 
 if __name__ == "__main__":
     main()
--- server.py
+++ server.py
@@ -3,7 +3,7 @@ from _thread import*
 import sys
 
 ##server address
-server = "192.167.1.1"
+server = "10.20.36.50"
 port = 5555
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -25,22 +25,22 @@ def read_pos(str):
 def make_pos(tup):
     return str(tup[0]) + "," + str(tup[1])
 
-pos = [(0, 0), (100, 100)]
+pos = [(0, 0), (0, 100)]
 
-def threaded_client(conn, player):
-    conn.send(str.encode(make_pos(pos[player])))
+def threaded_client(conn, playerCount):
+    conn.send(str.encode(make_pos(pos[playerCount])))
     reply = " "
     while True:
         ##recieve data from player if getting error increase size
         try:
             data = read_pos(conn.recv(2048).decode())
-            pos[player] = data
+            pos[playerCount] = data
 
             if not data:
                 print("Disconnected")
                 break
             else:
-                if player == 1:
+                if playerCount == 1:
                     reply = pos[0]
                 else:
                     reply = pos[1]

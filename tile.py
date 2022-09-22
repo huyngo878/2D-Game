5da85ea549aea84abfda553539d933ccd324bd0a
HuyNgo01 <90549599+HuyNgo01@users.noreply.github.com>
2022-09-21 22:10:18 -0500

    ORGANIZED

--- tile.py
+++
@@ -1,7 +0,0 @@
-from settings import *
-
-class Tile(pygame.sprite.Sprite):
-    def __init__(self, pos, groups):
-        super().__init__(groups)
-        self.image = pyame.image.load(os.path.join('maps', 'map1.png'))
-        self.rect = self.image.get_rect(topleft = pos)
